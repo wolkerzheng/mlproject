accdc2dda8d1a1ee6f88eaf9b2370fd3efd6a82b
zhengguidong <805115844@qq.com>
2017-02-19 20:06:32 +0800

    Add files via upload

---
+++ pca.py
@@ -0,0 +1,41 @@
+#encoding=utf8
+
+
+import  numpy as np
+
+def zeroMean(dataMat):
+    """
+    0均值化
+    datamat为数据集,没一行表示一个样本,没一列代表哦同一个
+    :param dataMat:
+    :return:
+    """
+    #按列求均值,每一列求均值后减去这个均值
+    meanVal = np.mean(dataMat,axis=0)
+    newData = dataMat - meanVal
+    return newData,meanVal
+
+def pca(dataMat,n):
+    newData, meanVal = zeroMean(dataMat)
+    covMat = np.cov(newData, rowvar=0)  # 求协方差矩阵,return ndarray；若rowvar非0，一列代表一个样本，为0，一行代表一个样本
+
+    eigVals, eigVects = np.linalg.eig(np.mat(covMat))  # 求特征值和特征向量,特征向量是按列放的，即一列代表一个特征向量
+    eigValIndice = np.argsort(eigVals)  # 对特征值从小到大排序
+    n_eigValIndice = eigValIndice[-1:-(n + 1):-1]  # 最大的n个特征值的下标
+    n_eigVect = eigVects[:, n_eigValIndice]  # 最大的n个特征值对应的特征向量
+    lowDDataMat = newData * n_eigVect  # 低维特征空间的数据
+    reconMat = (lowDDataMat * n_eigVect.T) + meanVal  # 重构数据
+    return lowDDataMat, reconMat
+
+
+def percentage2n(eigVals,percentage):
+    sortArray=np.sort(eigVals)   #升序
+    sortArray=sortArray[-1::-1]  #逆转，即降序
+    arraySum=sum(sortArray)
+    tmpSum=0
+    num=0
+    for i in sortArray:
+        tmpSum+=i
+        num+=1
+        if tmpSum>=arraySum*percentage:
+            return num  
